f6ae71f4a46334cead061f498c6863d9389056a3
matanco <matan@numgames.com>
2013-12-04 19:06:04 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsPoker::Application.config.secret_token = 'fdf912625a648b58c1498eb067317b2621043911e350e1f9989d8b189d8d79e43866288218bc78ec3398fbcb097afff08bbc236adc6916faa1bb104e1afd00f7'
